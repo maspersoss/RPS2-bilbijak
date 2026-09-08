9e0b9403ca43c61347be8600cbd7fd3a6d844c37
maspersoss <bilbijakristian@gmail.com>
2026-09-08 10:09:57 +0000

    nimam sporocila

--- prvi/run.py
+++ prvi/run.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 APP_ADDRESS = "0.0.0.0"
 APP_PORT = 5000
 
+
+@app.route("/", methods = ["GET", "POST"])
+def hello_world():
+    return render_template("index.html")
+
 app.config["DEBUG"] = True
 app.run(host=APP_ADDRESS, port=APP_PORT)
-
-@app.route("/")
-def hello_world():
-    return "Hello, 2.RI"
